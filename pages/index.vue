8cfb1c5e748cad9d04de048f082c16c6cfc5c80c
ParkerCatz <tksm.webdesign09@gmail.com>
2021-03-09 22:02:28 +0900

    add: SmallCardコンポーネントの追加

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,22 @@
       :key="cardInfo.id"
       :cardsSection="cardInfo"
     />
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
   </div>
 </template>
 
 <script>
-import { largeCardSections } from '@/assets/data.js'
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
 export default {
   data() {
-    return { largeCardInfo: largeCardSections }
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections,
+    }
   },
 }
 </script>
